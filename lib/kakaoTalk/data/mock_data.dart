18aac30edd8b6db95a21515e019ea81af3b08e06
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-25 22:38:20 +0900

    카카오톡 채팅 목업데이터

---
+++ lib/kakaoTalk/data/mock_data.dart
@@ -0,0 +1,28 @@
+String mock ='''
+[
+  {
+    "title" : "아이유",
+    "sub_title" : "아이유사랑입니다.",
+    "time" : "어제",
+    "images_url" : "https://i.namu.wiki/i/4Mlxj6PmC-VGpH89-MVlhAEezBnrd5vMiYjF6HOEWEyIPeui5oSLYgRyqaOlMKy4Ss0jSz1LZBxkP549NvOsWA.webp"
+  },
+  {
+    "title" : "아이유1",
+    "sub_title" : "아이유사랑입니다2",
+    "time" : "오후 9:40",
+    "images_url" : "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/202305/12/e53dd2d7-5c8f-42c1-bc2a-2a12157191b1.jpg"
+  },
+  {
+    "title" : "아이유2",
+    "sub_title" : "아이유사랑입니다3",
+    "time" : "오전 9:45",
+    "images_url" : "https://file2.nocutnews.co.kr/newsroom/image/2023/09/16/202309160233051464_0.jpg"
+  },
+  {
+    "title" : "아이유4",
+    "sub_title" : "아이유사랑입니다4",
+    "time" : "오후 9:42",
+    "images_url" : "https://img.sbs.co.kr/newsnet/etv/upload/2023/08/16/30000868871_500.jpg"
+  }
+]
+''';
